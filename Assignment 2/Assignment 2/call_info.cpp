0867d6bf3efc9e0ceaf6423ed118874d8da5ef18
Dalton McClain <soulsaver101@gmail.com>
2018-09-03 16:07:22 -0400

    2nd Final Draft

--- Assignment 2/Assignment 2/call_info.cpp
+++ Assignment 2/Assignment 2/call_info.cpp
@@ -1,15 +1,16 @@
-/****SAMPLE PROGRAM HEADER*******************************************************
-Dalton McClain           Total Points:  10
-Due Date:  8/30/18
+/****PROGRAM HEADER*******************************************************
+Dalton McClain           Total Points:  20
+Due Date:  9/6/18
 Course:  C0P3014
-Assignment:  Assignment 1
+Assignment:  Assignment 2
 Professor: Dr. Lofton Bullard
 Description: My program calculates the amount of money it costs to make a call, dependent on the amount of relays and the duration of the call.
-The program takes the users input and prints the calculations to the screen.
-I added a few functions to make the main function a little less overwhelmed, unnecessary but I felt up to the challenge.
-I added some graphics to make it more fun for the user to see and to direct the eyes to what I want the user to see.
+The program takes the users input and prints the calculations to the screen. It has three primary functions:
+Input - Takes the users input and saves the input to the address of the variables passed through the function parameters.
+Process - Takes the users input values and performs the required calculations on the data.
+Output - Prints the calculated data to the screen.
 
-
+The main function calls each of the primary functions and then after it goes through the functions it asks if the user wants to perform another calculation.
 *************************************************************/
 #include <iostream>
 #include <string>
@@ -24,6 +25,7 @@ void Output(string cell_num, double relays, double call_length, double net_cost,
 
 //Main function (No calculations, or printing, just define the variables
 int main() {
+	//Initial variables
 	string cell_num = "";
 	double relays, call_length, net_cost, call_tax, total_cost = 0.0;
 	
@@ -35,6 +37,7 @@ int main() {
 	//Variables for user input on wanting to make another calculation
 	string userInput = "";
 	bool flag = true;
+	bool errorCheck = true;
 
 	//Ask if the user wants to make another calculation
 	do {
@@ -42,18 +45,29 @@ int main() {
 		Input(cell_num, relays, call_length, net_cost, call_tax, total_cost);
 		Process(relays, call_length, net_cost, call_tax, total_cost);
 		Output(cell_num, relays, call_length, net_cost, call_tax, total_cost);
+		
+		//User correction
+		do {
+			//Ask for user input
+			cout << "\n\nDo you want to make another calculation? (Y/N): ";
+			cin >> userInput;
+			cout << endl;
 
-		//Ask for user input
-		cout << "\n\nDo you want to make another calculation? (Y/N): ";
-		cin >> userInput;
-		cout << endl;
+			if (userInput == "Y" || userInput == "y") {
+				flag = true;
+				errorCheck = false;
+			}
+			else if (userInput == "N" || userInput == "n") {
+				flag = false;
+				errorCheck = false;
+			}
+			else {
+				cout << "\n\nPlease enter either a Y or N . . .\n\n";
 
-		if (userInput == "Y" || userInput == "y") {
-			flag = true;
-		}
-		else if (userInput == "N" || userInput == "n") {
-			flag = false;
-		}
+				errorCheck = true;
+			}
+		} while (errorCheck);
+
 	} while (flag);
 
 	return 0;
